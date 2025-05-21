b9dcf12454ec3306c18efe6b3e511835c056459e
sephvelasco <marcusjoseph14@gmail.com>
2025-05-22 00:26:27 +0800

    Update bestmodel.py

--- bestmodel.py
+++ bestmodel.py
@@ -3,21 +3,21 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('best_model.h5')
+    model = tf.keras.models.load_model('finalsmodel.h5')
     return model
 
 model = load_model()
 
 st.write("""
-# Weather Classification System""")
-file = st.file_uploader("Choose weather photo from computer", type=["jpg", "png"])
+# VELASCO FINAL EXAM""")
+file = st.file_uploader("Upload weather photo", type=["jpg", "png"])
 
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
 
 def import_and_predict(image_data, model):
-    size = (28, 28)  
+    size = (64, 64)  
     image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
     img = np.asarray(image).astype(np.float32) / 255.0  
     img_reshape = np.expand_dims(img, axis=0)
